cfe087e263e51a11f992b62d34dee2974975d55d
Javier Chacnama <jchacnama.q@gmail.com>
2025-01-09 23:28:04 -0500

    Guardar datos en la base de datos

--- sesion05/ejemplo-consumo-apis/lib_randomuser.py
+++ sesion05/ejemplo-consumo-apis/lib_randomuser.py
@@ -2,9 +2,11 @@ import requests
 import mysql.connector
 
 class RandomUserApi:
+    # Metodo constructor
     def __init__(self,api_url="https://randomuser.me/api"):
         self.api_url = api_url
-
+    
+    # Metodos
     def get_users(self,results=1):
         url = self.api_url + f"?results={results}&nat=es&noinfo"
         response = requests.get(url)
@@ -13,3 +15,35 @@ class RandomUserApi:
         else:
             print(f"Error: {response.status_code}")
             return []
+    def insert_users_to_db(self,users):
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='mysqljavier',
+            database='datag3'
+        )
+        
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS usuarios (
+                                    id INT AUTO_INCREMENT PRIMARY KEY,
+                                    nombre VARCHAR(255),
+                                    pais VARCHAR(255),
+                                    correo VARCHAR(255),
+                                    telefono VARCHAR(255),
+                                    foto VARCHAR(255)
+                            )
+                            """)
+        for user in users:
+            cursor.execute("""
+                            INSERT INTO usuarios (nombre, pais, correo, telefono, foto)
+                            VALUES (%s, %s, %s, %s, %s)
+                            """, (
+                                f"{user['name']['first']} {user['name']['last']}",
+                                user['location']['country'],
+                                user['email'],
+                                user['phone'],
+                                user['picture']['large']
+                            ))
+            connection.commit()
--- sesion05/ejemplo-consumo-apis/main.py
+++ sesion05/ejemplo-consumo-apis/main.py
@@ -3,11 +3,18 @@ from lib_randomuser import RandomUserApi
 if __name__ == "__main__":
     api = RandomUserApi()
     results = int(input("Cuanros usuarios quiere obtener? "))
-    data = api.get_users(results)
-    for user in data:
+    users = api.get_users(results)
+    for user in users:
         print(f"Nombe: {user['name']['first']} {user['name']['last']}")
         print(f"Email: {user['email']}")
         print(f"Telefono: {user['phone']}")
         print(f"Pais: {user['location']['country']}")
         print(f"Foto: {user['picture']['large']}")
         print("="*40)
+
+    respuesta = input("Desea guardar los datos en la base de datos? (s/n): ")
+    if respuesta.lower() == 's':
+        api.insert_users_to_db(users)
+        print("Datos guardados en la base de datos.")
+    else:
+        print("Datos no guardados.")
